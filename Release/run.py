cbf0182e8ea5f49fd852247a6d1ba21170145cae
XZ-CG <lixizhi0504@gmail.com>
2021-09-23 18:35:46 +0800

    Update run.py

--- Release/run.py
+++ Release/run.py
@@ -10,22 +10,20 @@ import datetime
 import numpy as np
 import re
 
-output_path = os.path.abspath(os.path.join(os.path.dirname("__file__"),os.path.pardir))+"/asteroid/point1/Assets"+"/"
+output_path = os.path.abspath(os.path.join(os.path.dirname("__file__"), 
+                                           os.path.pardir))+"/asteroid/point1/Assets"+"/"
 
-#----------------------Launch the exe file (execute on GPU side)----------------------------
+#--------------------Launch the exe file ( will execute on GPU side)------------------------
 def compute(i): 
     name = "point%s"%i
     path = os.getcwd()
     file = path + "\\Asteroid.exe"
-    subprocess.call([file,name])
-    return 1.0    
+    subprocess.call([file, name])
 
 if __name__ == "__main__":
     start_time = datetime.datetime.now().time().strftime('%H:%M:%S')
-    # create_h_file("Bennu.spheres", metaball=True)
-    # create_h_file("output.spheres", metaball=True)
     
-    print(compute(1))
+    compute(1)
     
     end_time = datetime.datetime.now().time().strftime('%H:%M:%S')
     total_time=(datetime.datetime.strptime(end_time,'%H:%M:%S') - datetime.datetime.strptime(start_time,'%H:%M:%S'))
